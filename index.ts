1332747a5aa3585fb9bd2b9fe3dfb579b220ea7e
Igor Bari <mucsi96@gmail.com>
2024-01-07 22:03:40 +0100

    download pdf

--- index.ts
+++ index.ts
@@ -1,10 +1,32 @@
-import { promises } from "fs";
+import { readFile, stat, mkdir } from "fs/promises";
+import { finished } from "stream/promises";
 import { PDFDocument } from "pdf-lib";
+import { createWriteStream } from "fs";
+import { Readable } from "stream";
+import { dirname, join } from "path";
+
+async function exists(file: string) {
+  try {
+    await stat(file);
+    return true;
+  } catch {
+    return false;
+  }
+}
 
 async function main(name: string, url: string) {
-  const fetchResponse = await fetch(url);
-  await promises.writeFile(name, await fetchResponse.arrayBuffer());
-  const pdfData = await promises.readFile(name);
+  const path = join(__dirname, ".cache", name);
+  if (!await exists(path)) {
+    if (!await exists(dirname(path))) await mkdir(dirname(path));
+
+    const fetchResponse = await fetch(url);
+    if (!fetchResponse.body) {
+      throw new Error("Response had no body");
+    }
+    const dest = createWriteStream(path);
+    await finished(Readable.fromWeb(fetchResponse.body).pipe(dest));
+  }
+  const pdfData = await readFile(path);
   const pdfDoc = await PDFDocument.load(pdfData);
   const pages = pdfDoc.getPages();
   console.log(pages);
