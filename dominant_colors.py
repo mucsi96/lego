75ed6510386779dc5e9ad94e63a78a9952e70d45
Igor Bari <mucsi96@gmail.com>
2024-01-26 08:31:54 +0100

    draw dominant colors with pyplot piechart

--- dominant_colors.py
+++ dominant_colors.py
@@ -1,30 +1,28 @@
 from __future__ import print_function
 from __future__ import division
 import cv2 as cv
+from matplotlib import pyplot
 import numpy as np
 from sklearn.cluster import KMeans
 
-n_colors = 3
+n_colors = 6
 preview_size = 300
 
-src = cv.imread(cv.samples.findFile("images/11_2_1x_X10.jpeg"))
+src = cv.imread(cv.samples.findFile("images/11_2_1x_X9.jpeg"))
 
 # image = cv.cvtColor(src, cv.COLOR_BGR2HSV)
 image = src
-# reshape the image to be a list of pixels
-image = src.reshape((image.shape[0] * image.shape[1], 3))
-clusters = KMeans(n_clusters=n_colors)
-clusters.fit_predict(image)
-colors = np.array([clusters.cluster_centers_], dtype=np.uint8)
-# colors = cv.cvtColor(colors, cv.COLOR_HSV2BGR)
-colors_image = np.zeros((preview_size, preview_size * n_colors, 3), dtype=np.uint8)
-for i in range(n_colors):
-    cv.rectangle(
-        colors_image,
-        (i * preview_size, 0),
-        ((i + 1) * preview_size, preview_size),
-        tuple(colors[0, i].tolist()),
-        cv.FILLED,
-    )
-cv.imshow("colors", colors_image)
-cv.waitKey(0)
+image = src.reshape((-1, 3))
+image = np.float32(image)
+clusters = KMeans(n_clusters=n_colors, random_state=0)
+labels = clusters.fit_predict(image)
+bincount = np.bincount(labels)
+colors = clusters.cluster_centers_.astype(np.uint8)
+rgba_colors = [(color[2] / 255.0, color[1] / 255.0, color[0] / 255.0, 1) for color in colors]
+
+sorted_indices = sorted(range(len(bincount)), key=lambda k: bincount[k], reverse=True)
+sorted_labels = [bincount[i] for i in sorted_indices]
+sorted_colors = [rgba_colors[i] for i in sorted_indices]
+
+pyplot.pie(sorted_labels, None, None, sorted_colors)
+pyplot.show()
